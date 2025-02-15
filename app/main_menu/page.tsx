87f2be1f3ed595570ca11e128127741f2a01d729
KowaiPanda <87180631+KowaiPanda@users.noreply.github.com>
2025-02-15 20:11:53 +0000

    added a menu page

---
+++ app/main_menu/page.tsx
@@ -0,0 +1,11 @@
+import Logo from '../components/logo';
+
+export default function Page() {
+
+    return (
+        <div className="flex flex-col items-center top-0 h-screen bg-startbackgroud bg-cover">
+            <Logo />
+        </div>
+    );
+
+}
